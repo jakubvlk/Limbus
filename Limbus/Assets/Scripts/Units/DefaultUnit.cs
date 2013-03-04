52c0fed309045ddce7ea83897e0731365d5f931a
Jakub Vlk <jakvlk@seznam.cz>
2013-03-04 18:34:28 +0200

    Main commit

---
+++ Limbus/Assets/Scripts/Units/DefaultUnit.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class DefaultUnit : MonoBehaviour {
+	
+	float lifes;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		lifes = 100f;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Limbus/Assets/Scripts/Units/ExtendedUnit.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+
+public class ExtendedUnit : MonoBehaviour {
+	
+	public float power, cost, range;
+	
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+	
+	void Fire ()
+	{
+		throw new System.NotImplementedException ();
+	}
+
+	void Aim ()
+	{
+		throw new System.NotImplementedException ();
+	}
+	
+	void Explode ()
+	{
+		throw new System.NotImplementedException ();
+	}
+}
