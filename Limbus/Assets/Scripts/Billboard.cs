c2615e5f93da35efa7ca2b3b53da0e66fec1f142
Jakub Vlk <jakvlk@seznam.cz>
2013-04-24 19:02:32 +0300

    billboarding of ranks

--- Limbus/Assets/Scripts/Billboard.cs
+++ Limbus/Assets/Scripts/Billboard.cs
@@ -4,16 +4,21 @@ using System.Collections;
 public class Billboard : MonoBehaviour
 {
 	private Transform myTransform;
+	private Camera mainCamera;
 	
 	void Start ()
 	{
 		myTransform = transform;
+		mainCamera = Camera.mainCamera;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		
+		myTransform.LookAt(mainCamera.transform.position);
+		Quaternion tmpRot = myTransform.rotation;
+		tmpRot.x = tmpRot.z = 0;		
+		myTransform.rotation = tmpRot;
 	}
 }
 	
